4e5c40662c7a2c2289a449756a57e26e688d1fea
Corrine <corrine.sing@gmail.com>
2023-05-11 15:45:12 +0100

    another quick change

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TitleBar = ({handleSelectChange, genres}) =>{
   return (
     <div>
-      <h1>Codeclan's Top 20 Shit Songs</h1>
+      <h1>Codeclans Top 20 Shit Songs</h1>
       <select onChange={handleSelectChange}>
         {genres.map(genre => {
           return <option key={genre.name} value={genre.url}>{genre.name}</option>
