2fb6e70a615fec86b492644e8ee510e4c8b681f6
Corrine <corrine.sing@gmail.com>
2023-05-11 15:29:21 +0100

    add deploy job

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TitleBar = ({handleSelectChange, genres}) =>{
   return (
     <div>
-      <h1>Codeclan Hit Parade</h1>
+      <h1>Codeclan Top 20 Shit Songs</h1>
       <select onChange={handleSelectChange}>
         {genres.map(genre => {
           return <option key={genre.name} value={genre.url}>{genre.name}</option>
